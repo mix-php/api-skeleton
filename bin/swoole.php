9eb5fe4b4444ec02023bd243db6e1f20ca7d6cdb
liujian <onanying@126.com>
2021-07-15 11:46:41 +0800

    feat:add worker_num

--- bin/swoole.php
+++ bin/swoole.php
@@ -12,5 +12,8 @@ define("APP_DEBUG", $_ENV['APP_DEBUG']);
 $vega = Vega::new();
 $http = new Swoole\Http\Server('0.0.0.0', 9501);
 $http->on('Request', $vega->handler());
+$http->set([
+    'worker_num' => 4,
+]);
 Logger::instance()->info('Start swoole server');
 $http->start();
