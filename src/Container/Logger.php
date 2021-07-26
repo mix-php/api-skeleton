3c6a029ab783a0f7144ca8b0b214da289c239671
liujian <onanying@126.com>
2021-07-26 19:19:30 +0800

    feat:up logger

--- src/Container/Logger.php
+++ src/Container/Logger.php
@@ -2,6 +2,7 @@
 
 namespace App\Container;
 
+use Monolog\Formatter\LineFormatter;
 use Monolog\Handler\HandlerInterface;
 use Monolog\Handler\RotatingFileHandler;
 
@@ -21,8 +22,10 @@ class Logger implements HandlerInterface
     {
         if (!isset(self::$instance)) {
             $logger = new \Monolog\Logger('MIX');
-            $logger->pushHandler(new RotatingFileHandler(__DIR__ . '/../../runtime/logs/mix.log', 7));
-            $logger->pushHandler(new Logger());
+            $rotatingFileHandler = new RotatingFileHandler(__DIR__ . '/../../runtime/logs/mix.log', 7);
+            $rotatingFileHandler->setFormatter(new LineFormatter("[%datetime%] %channel%.%level_name%: %message% %context% %extra%\n", 'Y-m-d H:i:s.u'));
+            $logger->pushHandler($rotatingFileHandler);
+            PHP_SAPI == 'cli' and $logger->pushHandler(new Logger());
             self::$instance = $logger;
         }
         return self::$instance;
@@ -35,9 +38,7 @@ class Logger implements HandlerInterface
 
     public function handle(array $record): bool
     {
-        if (PHP_SAPI == 'cli') {
-            printf("%s  %s  %s\n", $record['datetime']->format('Y-m-d H:i:s.u'), $record['level_name'], $record['message']);
-        }
+        printf("%s  %s  %s\n", $record['datetime']->format('Y-m-d H:i:s.u'), $record['level_name'], $record['message']);
         return false;
     }
 
